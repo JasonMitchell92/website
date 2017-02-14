872a26e51f4f949c0a35a536547eb7af8afe9b45
JasonMitchell92 <jasonmitchell.jm92@gmail.com>
2017-02-14 13:53:37 -0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -96,7 +96,18 @@ $(function() {
 			draggable: false,
 			scrollwheel: false,
 			center: new google.maps.LatLng(36.1627, -86.7816),
-			disableDefaultUI: true
+			disableDefaultUI: true,
+			disableDoubleClickZoom: true,
+			keyboardShortcuts: false,
+			styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},
+				{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},
+				{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},
+				{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},
+				{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},
+				{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},
+				{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},
+				{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},
+				{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
 		})
 	}
 	google.maps.event.addDomListener(window, 'load', initialize);
