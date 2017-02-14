645fd82f3e6ebd074a6916f16ab9ef2a45562537
JasonMitchell92 <jasonmitchell.jm92@gmail.com>
2017-02-14 13:55:30 -0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -99,15 +99,7 @@ $(function() {
 			disableDefaultUI: true,
 			disableDoubleClickZoom: true,
 			keyboardShortcuts: false,
-			styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},
-				{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},
-				{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},
-				{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},
-				{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},
-				{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},
-				{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},
-				{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},
-				{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
+			styles: [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
 		})
 	}
 	google.maps.event.addDomListener(window, 'load', initialize);
